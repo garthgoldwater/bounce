a68f65d3955e844db81395b5972f7f7121421a8b
Garth Goldwater <garth.goldwater@gmail.com>
2015-03-05 04:33:57 -0500

    add world's most random styling to experiment with jeet

--- messages/client/message.js
+++ messages/client/message.js
@@ -2,6 +2,6 @@ Template.message.helpers({
   replies: function(){
     if(Session.get("selected")) {
       return Messages.find({parentId: Session.get("selected")});
-    } else return null;
-  },
+    } else { return false; }
+  }
 });
