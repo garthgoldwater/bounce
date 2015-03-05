24399ac7194fdfc2ea9710ca4cdb7443fed37566
Garth Goldwater <garth.goldwater@gmail.com>
2015-03-04 20:39:15 -0500

    add message show pane

--- messages/client/messages.js
+++ messages/client/messages.js
@@ -1,5 +1,15 @@
 Template.messages.helpers({
   messages: function() {
-    return Messages.find()
+    return Messages.find();
   },
+  selectedMessage: function() {
+    return Messages.findOne(Session.get("selected"));
+  }
+});
+
+Template.messages.events({
+  "click .message": function(event, template){
+    Session.set("selected", this._id);
+    console.log("you clicked " + this._id);
+  }
 });
